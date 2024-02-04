caa8f665d7dfc1bf50b3325872f18548e60872fe
DRossigalli <dirossigalli@gmail.com>
2024-02-04 16:17:20 -0300

    Bug fix

--- src/include.js
+++ src/include.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // include.js
 // document.addEventListener("DOMContentLoaded", function() {
 //     var xhttp = new XMLHttpRequest();
@@ -28,3 +29,35 @@ document.addEventListener("DOMContentLoaded", function() {
     xhttp.open("GET", "../src/header.html", true);
     xhttp.send();
 });
+=======
+// include.js
+// document.addEventListener("DOMContentLoaded", function() {
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             document.getElementsByTagName("header")[0].firstChild = this.responseText;
+//         }
+//     };
+//     xhttp.open("GET", "../src/header.html", true);
+//     xhttp.send();
+// });
+
+
+// include.js
+document.addEventListener("DOMContentLoaded", function() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var bodyContent = document.createElement("div");
+            bodyContent.innerHTML = this.responseText;
+            var body = document.getElementsByTagName("body")[0];
+            // Insere o conteúdo como o primeiro filho da tag <body>
+            while (bodyContent.firstChild) {
+                body.insertBefore(bodyContent.firstChild, body.firstChild);
+            }
+        }
+    };
+    xhttp.open("GET", "../src/header.html", true);
+    xhttp.send();
+});
+>>>>>>> fc38d56 (Bug fixes)
